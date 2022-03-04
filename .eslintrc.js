919d927def21fb3974222f007da57484a6dca4b8
ChrissiQ <1127719+ChrissiQ@users.noreply.github.com>
2022-03-04 14:10:59 -0700

    🎨   Config linting and code style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: ["@remix-run/eslint-config"],
+  extends: ["@remix-run/eslint-config", "plugin:prettier/recommended"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "arrow-body-style": "off", // https://github.com/prettier/eslint-plugin-prettier/issues/65
+    "prefer-arrow-callback": "off", // https://github.com/prettier/eslint-plugin-prettier/issues/65
+  },
 };
